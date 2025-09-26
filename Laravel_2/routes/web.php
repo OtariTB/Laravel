b1db6ec62324db21f4ba3480b27d2a5cfa37a12c
LAB321 Student05 <lab321student05@gau.local>
2025-09-26 13:46:07 +0400

    Lecture 2.2

--- Laravel_2/routes/web.php
+++ Laravel_2/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test1', function () {
+    return '<h1>Hellow Mr Laraver</h1>';
+});
+
+Route::get('/main', function () {
+    return view('home');
+});
+
+Route::view('/main', 'home', ['name'=> 'Otari', 'lastname'=> 'Tabatadze']);
