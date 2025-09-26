a0349a61710e8c098caaec3ae5037b09ff89464e
LAB321 Student05 <lab321student05@gau.local>
2025-09-26 12:52:36 +0400

    Lecture2

---
+++ Laravel_2025_2/resources/views/skola.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>School</title>
+    <style>
+        .center-text {
+            font-size: 36px;
+            font-weight: bold;
+            color: #333;
+            padding: 20px;
+            border: 2px solid #333;
+            border-radius: 10px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <a href="{{ url('/') }}">Home</a>
+    <h1 class="center-text">161-e Sajaro Skola</h1>
+    <a href="{{ url('/skola') }}">Skola</a>
+    <a href="{{ url('/universiteti') }}">Universiteti</a>
+</body>
+</html>
